586ef701e6a531fd8a6a3d0e41fdd5e1dcd168a0
bwty <whalelephant@users.noreply.github.com>
2023-07-23 00:09:59 +0200

    fail rand compile

--- packages/halo2-verifier/src/lib.rs
+++ packages/halo2-verifier/src/lib.rs
@@ -1,2 +1,33 @@
-pub mod error;
-pub mod proof_system;
+use pse_halo2wrong::halo2::{
+    halo2curves::bn256::Fr,
+    halo2curves::{
+        bn256::{Bn256, G1Affine, G1},
+        group::cofactor::CofactorCurve,
+    },
+    plonk::{verify_proof, Error, VerifyingKey},
+    poly::kzg::{
+        commitment::{KZGCommitmentScheme, ParamsKZG},
+        multiopen::VerifierGWC,
+        strategy::SingleStrategy,
+    },
+    transcript::{Blake2bRead, Challenge255, TranscriptReadBuffer},
+};
+
+/// Verify Halo2 Proof
+pub fn halo2_verify(
+    params: &ParamsKZG<Bn256>,
+    vk: &VerifyingKey<<G1 as CofactorCurve>::Affine>,
+    proof: &[u8],
+    public_inputs: &[Fr],
+) -> Result<(), Error> {
+    let strategy = SingleStrategy::new(params);
+    let mut transcript = Blake2bRead::<_, _, Challenge255<_>>::init(proof);
+
+    verify_proof::<
+        KZGCommitmentScheme<Bn256>,
+        VerifierGWC<'_, Bn256>,
+        Challenge255<G1Affine>,
+        Blake2bRead<&[u8], G1Affine, Challenge255<G1Affine>>,
+        SingleStrategy<'_, Bn256>,
+    >(params, vk, strategy, &[&[public_inputs]], &mut transcript)
+}
